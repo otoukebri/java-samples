9237056b58d22b5efe81a5f9f055ff259155a74c
Oussema TOUKEBRI <oussema.toukebri@gmail.com>
2017-01-03 22:02:10 +0100

    feat(rabbitmq-client) init project

---
+++ rabbitmq-consumer/src/main/java/rabbitmq_consumer/tn/zelda/projects/java/App.java
@@ -0,0 +1,13 @@
+package rabbitmq_consumer.tn.zelda.projects.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
