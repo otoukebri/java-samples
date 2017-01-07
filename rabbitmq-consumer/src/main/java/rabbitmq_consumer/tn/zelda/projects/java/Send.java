3e745249619e60605c6e94b5a3646a7a96c2c66f
Oussema TOUKEBRI <oussema.toukebri@gmail.com>
2017-01-07 21:24:26 +0100

    feat(rabbitmq-client) add rabbitmq sender + readme +  init script

--- rabbitmq-consumer/src/main/java/rabbitmq_consumer/tn/zelda/projects/java/Send.java
+++ rabbitmq-consumer/src/main/java/rabbitmq_consumer/tn/zelda/projects/java/Send.java
@@ -1,27 +1,23 @@
 package rabbitmq_consumer.tn.zelda.projects.java;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 public class Send {
 
-  private final static String QUEUE_NAME = "rabbitmq-example";
+	private final static String QUEUE_NAME = "my-rabbit-q1";
 
-  public static void main(String[] argv) throws Exception {
-    ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("localhost");
-    factory.setUsername("admin");
-    factory.setPassword("admin");
-    factory.setPort(15672);
-    Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
+	public static void main(String[] argv) throws Exception {
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
 
-    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-    String message = "Hello World!";
-    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-    System.out.println(" [x] Sent '" + message + "'");
-    channel.close();
-    connection.close();
-  }
+		channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+		String message = "Hello World!!***";
+		channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+		System.out.println(" [x] Sent '" + message + "'");
+		channel.close();
+		connection.close();
+	}
 }
-
